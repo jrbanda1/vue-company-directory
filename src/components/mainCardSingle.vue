09c05bff69c3205a00c972f8b33aebfee495eb7b
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-09-16 23:01:35 -0500

    added style scoped to .card

--- src/components/mainCardSingle.vue
+++ src/components/mainCardSingle.vue
@@ -7,8 +7,20 @@ const fullName = firstName +' ' + lastName;
 
 <template>
     <div class="card">
-        <img :src="faker.internet.avatar()" alt="" srcset="" />
-        <p class="">{{ fullName }}</p>
-        <p class="">{{ faker.internet.exampleEmail(firstName, lastName) }}</p>
+        <div class="card-image">
+            <img :src="faker.internet.avatar()" alt="" srcset=""/>
+        </div>
+        <div class="card-details">
+            <p class="card-details-name">{{ fullName }}</p>
+            <p class="card-details-job">{{ faker.name.jobTitle() }}</p>
+            <p class="card-details-email">{{ faker.internet.exampleEmail(firstName, lastName) }}</p>
+        </div>
+            <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
     </div>
 </template>
+
+<style scoped lang="postcss">
+.card{
+    @apply cursor-pointer overflow-hidden rounded-xl bg-slate-100 p-8 shadow-md;
+}
+</style>
