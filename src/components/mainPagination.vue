b0e9cf3df32a8f8206b6b7fbd4dd3e4a708a7d0e
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-09-17 13:21:28 -0500

    created pagination

--- src/components/mainPagination.vue
+++ src/components/mainPagination.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-const pages = ref(10)
+const pages = ref(5)
 const activePage = ref(4)
 
 const prevPage = () => {
@@ -16,11 +16,7 @@ const nextPage = () => {
 </script>
 
 
-<template>
-    <pre>
-        Current Page is {{ activePage }}
-    </pre>
-    
+<template> 
     <div class="pagination">
         <button class="action" :disabled="activePage === 1" @click="prevPage">Prev</button>
         <button class="page" v-for="page in pages" :class="page === activePage ? 'active' : ''" @click="activePage = page">
@@ -29,3 +25,18 @@ const nextPage = () => {
         <button class="action" :disabled="activePage === 1" @click="nextPage">Next</button>
     </div>
 </template>
+
+<style lang="postcss" scoped>
+  .pagination {
+    @apply flex justify-center gap-4;
+    .action {
+      @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200 disabled:text-slate-400 hover:disabled:bg-slate-100;
+    }
+    .page {
+      @apply rounded-md bg-slate-300 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200;
+      &.active {
+        @apply bg-yellow-700 text-slate-100 hover:bg-yellow-600;
+      }
+    }
+  }
+</style>
