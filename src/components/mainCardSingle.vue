439d4d48b675cadbdcbeb44c9ca1ed671e87c0a1
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-09-17 12:11:49 -0500

    added cards/video min 35:34

--- src/components/mainCardSingle.vue
+++ src/components/mainCardSingle.vue
@@ -17,7 +17,8 @@ const selectCard = () => {
         </div>
         <div class="card-details">
             <p class="card-details-name">{{ fullName }}</p>
-            <p class="card-details-job">{{ faker.name.jobTitle() }}</p>
+            <p class="card-details-job">{{ faker.name.jobTitle()}}, 
+            {{ faker.name.jobArea() }}</p>
             <p class="card-details-email">{{ faker.internet.exampleEmail(firstName, lastName) }}</p>
         </div>
             <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
@@ -35,6 +36,21 @@ const selectCard = () => {
              @apply mx-auto rounded-full object-contain
         }
        
+    }
+    &-details{
+        @apply flex flex-col pt-6 text-center;
+        &-name{
+            @apply text-3xl font-thin tracking-wider text-yellow-800;
+        }
+        &-job{
+            @apply text-xs  text-red-800;
+        }
+        &-email{
+            @apply text-xs text-slate-700 font-bold;
+        }
+        &-quote{
+            @apply text-lg pt-4 italic text-slate-700;
+        }
     }
 }
 </style>
