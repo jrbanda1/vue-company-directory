e6719ce29ff49bf6ceea441e5654cc32ff063cfc
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-09-17 10:18:13 -0500

    fixed name variable passing by reference

--- src/components/mainCardSingle.vue
+++ src/components/mainCardSingle.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { faker } from '@faker-js/faker'
+
 const firstName = faker.name.firstName()
 const lastName = faker.name.lastName()
-const fullName = firstName +' ' + lastName;
+const fullName = `${ firstName } ${ lastName }`
+
+const selectCard = () => {
+    console.log(`${fullName} selected`)
+}
 </script>
 
 <template>
-    <div class="card">
+    <div class="card" @click="selectCard">
         <div class="card-image">
             <img :src="faker.internet.avatar()" alt="" srcset=""/>
         </div>
@@ -21,6 +26,6 @@ const fullName = firstName +' ' + lastName;
 
 <style scoped lang="postcss">
 .card{
-    @apply cursor-pointer overflow-hidden rounded-xl bg-slate-100 p-8 shadow-md;
+    @apply cursor-pointer overflow-hidden rounded-md bg-slate-100 p-8 shadow-md hover:scale-110 ;
 }
 </style>
