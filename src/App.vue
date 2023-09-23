7478c7e2fec6dcf7bf41f6de4cf2a9260fd99689
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-09-23 12:25:31 -0500

    onMounted

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
 import MainPage from '@/components/MainPage.vue'
+
+import { ref, onMounted } from 'vue'
+const showMainPage = ref(true)
+
+onMounted(() => {
+  window.location.pathname === '/' ? showMainPage.value = true : (showMainPage.value = false)
+ })
 </script>
 
 <template>
   <NavBar/>
-  <MainPage />
+  <button class="bg-orange-700 px-4 py-2" @click="showMainPage = !showMainPage"> Show Main Page</button>
+  <MainPage v-if="showMainPage" />
+  <div v-else>
+    <h1>This is another "Page"</h1>
+  </div>
 </template>
