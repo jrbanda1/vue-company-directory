dadfcfb13c2f22d9c51998c2f56f93c6f85fc81c
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-09-23 17:11:20 -0500

    added  router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
-import MainPage from '@/components/MainPage.vue'
 
 
 </script>
 
 <template>
   <NavBar/>
-  <button class="bg-orange-700 px-4 py-2" @click="showMainPage = !showMainPage"> Show Main Page</button>
-  <MainPage v-if="showMainPage" />
-  <div v-else>
-    <h1>This is another "Page"</h1>
-  </div>
+  <RouterView/>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import MainPage from '@/views/MainPage.vue'
-import LoginPage from '@/components/LoginPage.vue'
+import MainPage from '@/components/MainPage.vue'
+import Login from '@/components/Login.vue'
+import NotFound from '@/components/NotFound.vue'
 
 
 const routes = [
-  { path: '/', component: MainPage },
-  { path: '/lgin', component: LoginPage},
+  { path: '/', name: 'Home', component: MainPage },
+    { path: '/login', name: 'Login', component: Login },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ]
 
 export default createRouter({
