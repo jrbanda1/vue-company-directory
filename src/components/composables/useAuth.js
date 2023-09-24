f3feb6a0dca9830ef93e9ab272661d74ae7a9690
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-09-23 21:34:17 -0500

    useAuth.js file created

---
+++ src/components/composables/useAuth.js
@@ -0,0 +1,34 @@
+import { ref } from 'vue'
+
+const dbUsers = [
+    {
+        username: 'admin',
+        password: 'admin', 
+            name: 'Admin',
+            role: 'admin',
+            email:'admin@coolcompany.com',
+    }
+    {
+        username: 'user',
+        password: 'user', 
+            name: 'User',
+            role: 'user',
+            email:'user@coolcompany.com',
+    }
+]
+
+const isAuthenticated = ref(false)
+const user = ref({})
+
+
+
+export const useAuth = () => {
+    const login = (username, password) => {
+        const user = dbUsers.find((u) => u.username === username && u.password === password)
+        if (user) {
+            const { name, role, email} = user
+        isAuthenticated.value = true
+        user.value={name, role, email, username }
+    }
+    return { isAuthenticated, user, login }
+}
